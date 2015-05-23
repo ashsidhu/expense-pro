e807f850dedff283694e8aa31a5f3ec3f0f7aade
Ashmeet Sidhu <ashmeet.sidhu7@gmail.com>
2015-05-23 23:35:11 +0530

    add app configuration

--- server/config/index.js
+++ server/config/index.js
@@ -1,7 +1,10 @@
+'use strict';
+
 var morgan = require('morgan');
+var server = require('./server');
 
 function config (app) {
-  
+  app.set('clientPath', server.root + '/client');
   app.use(morgan('dev'));
 }
 
--- server/index.js
+++ server/index.js
@@ -1,11 +1,11 @@
+'use strict';
+
 var express = require('express');
 var config = require('./config');
+var routes = require('./routes');
 var app = express();
 
 config(app);
-
-app.use(function (req, res) {
-  res.sendStatus(200);
-})
+routes(app);
 
 app.listen(4000);
