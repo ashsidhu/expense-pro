f774f5f50352367b4c48514ffec4da54eb3b2f2e
Ashmeet Sidhu <ashmeet.sidhu7@gmail.com>
2015-05-24 01:56:29 +0530

    server configuration for body parsing

--- server/config/index.js
+++ server/config/index.js
@@ -1,9 +1,14 @@
 'use strict';
 
 var morgan = require('morgan');
+var bodyParser = require('body-parser');
+var cookieParser = require('cookie-parser');
 var server = require('./server');
 
 function config (app) {
+  app.use(bodyParser.urlencoded({ extended: false }));
+  app.use(bodyParser.json());
+  app.use(cookieParser());
   app.set('clientPath', server.root + '/client');
   app.use(morgan('dev'));
 }
