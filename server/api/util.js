bc0811a0b0295f42bcc74a8031a7a7853d949978
Ashmeet Sidhu <ashmeet.sidhu7@gmail.com>
2015-05-24 18:12:08 +0530

    response helpers

---
+++ server/api/util.js
@@ -0,0 +1,37 @@
+'use strict';
+
+var util = {};
+
+util.send200 = function (res, data) {
+  data = data || 'OK';
+  return res.status(200).json({
+    error: false,
+    data: data
+  });
+};
+
+util.send400 = function (res, message) {
+  message = message || 'Bad Request';
+  return res.status(400).json({
+    error: true,
+    data: message
+  });
+};
+
+util.send404 = function (res, message) {
+  message = message || 'Resource Not Found';
+  return res.status(404).json({
+    error: true,
+    data: message
+  });
+};
+
+util.send500 = function (res, message) {
+  message = message || 'Internal Server Error';
+  return res.status(500).json({
+    error: true,
+    data: message
+  });
+};
+
+module.exports = util;
