15cf7998b982a0e0667eb8a7bfc318248a111b92
Ashmeet Sidhu <ashmeet.sidhu7@gmail.com>
2015-05-26 00:45:35 +0530

    add scss support and browsersync

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,43 @@
 var gulp = require('gulp');
+var browserSync = require('browser-sync').create();
+var reload = browserSync.reload;
 var $ = require("gulp-load-plugins")({
   pattern: ['gulp-*', 'gulp.*'],
   replaceString: /\bgulp[\-.]/
 });
-// var nodeInspector = require('gulp-node-inspector');
+
+gulp.task('sass', function () {
+  return gulp.src(__dirname + '/client/styles/main.scss')
+    .pipe($.sass().on('error', $.sass.logError))
+    .pipe(gulp.dest(__dirname + '/client/css'))
+});
+
+gulp.task('sass:watch', function () {
+  gulp.watch(__dirname + '/client/styles/main.scss', ['sass']);
+});
+
+gulp.task('browserSync', function () {
+  return browserSync.init({
+    notify: false,
+    port: 4005,
+    ui: false,
+    proxy: 'localhost:4000'
+  });
+})
+
+gulp.task('public:watch', function () {
+  gulp.watch([
+    __dirname + '/client/**/*.js'
+  ]).on('change', reload);
+
+  gulp.watch([
+    __dirname + '/client/**/*.css'
+  ]).on('change', reload);
+
+  gulp.watch([
+    __dirname + '/client/**/*.html'
+  ]).on('change', reload);
+})
 
 gulp.task('inspector', function() {
   console.log('[info] Visit http://localhost:8080/debug?port=5858 to start debugging.')
@@ -21,4 +55,4 @@ gulp.task('server', function () {
   })
 });
 
-gulp.task('default', ['server', 'inspector']);
+gulp.task('default', ['sass', 'sass:watch', 'public:watch', 'browserSync', 'server', 'inspector']);
