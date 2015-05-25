1220d53206c36521d38b2541b3fa98fbbdd06104
Ashmeet Sidhu <ashmeet.sidhu7@gmail.com>
2015-05-25 10:49:14 +0530

    expense controller

---
+++ server/api/expense/expense.controller.js
@@ -0,0 +1,27 @@
+'use strict';
+
+var util = require('../util');
+
+var controller = {};
+
+controller.index = function (req, res) {
+
+};
+
+controller.create = function (req, res) {
+
+};
+
+controller.show = function (req, res) {
+
+};
+
+controller.update = function (req, res) {
+
+};
+
+controller.remove = function (req, res) {
+
+};
+
+module.exports = controller;
