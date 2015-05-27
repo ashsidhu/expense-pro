8461687b735c9b18726281de20424213b99829c9
Ashmeet Sidhu <ashmeet.sidhu7@gmail.com>
2015-05-27 19:43:15 +0530

    update and remove routes for expenses

--- server/api/expense/expense.controller.js
+++ server/api/expense/expense.controller.js
@@ -45,17 +45,57 @@ controller.create = function (req, res) {
     if (error.code === errors.PG_DECIMAL_RANGE) {
       return util.send400(res, errors.PG_DECIMAL_RANGE);
     }
-    // if (error.messagereturn util.send500(res, errors.SERVER_ERROR);
     return util.send500(res, errors.SERVER_ERROR);
   })
 };
 
 controller.update = function (req, res) {
-
+  return Expense.forge()
+  .query({where: {id: req.params.id}})
+  .fetch()
+  .then(function(expense){
+    if (!expense){
+      var error = new Error;
+      error.code = errors.NOT_FOUND
+      throw error
+    }
+    return expense.setAmount(req.body.amount)
+  }).then(function (expense) {
+    return expense.set({
+      description: req.body.description,
+      category: req.body.category,
+      date: req.body.date,
+      comment: req.body.comment
+    }).save();
+  }).then(function () {
+    return util.send200(res)
+  }).catch(function (error) {
+    if ([errors.PG_DECIMAL_RANGE, errors.NOT_FOUND].indexOf(error.code) !== -1) {
+      return util.send400(res, error.code);
+    }
+    return util.send500(res, errors.SERVER_ERROR);
+  })
 };
 
 controller.remove = function (req, res) {
-
+  return Expense.forge()
+  .query({where: {id: req.params.id}})
+  .fetch()
+  .then(function(expense){
+    if (!expense){
+      var error = new Error;
+      error.code = errors.NOT_FOUND
+      throw error
+    }
+    return expense.destroy();
+  }).then(function() {
+    return util.send200(res, errors.DESTROYED);
+  }).catch(function(error) {
+    if (error.code === errors.NOT_FOUND) {
+      return util.send400(res, 'id:' + req.params.id + ' not found');
+    }
+    return util.send500(res, errors.SERVER_ERROR);
+  })
 };
 
 module.exports = controller;
