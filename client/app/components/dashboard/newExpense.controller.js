94f07d0166e184b1392dc57662dfc854683f25dd
Ashmeet Sidhu <ashmeet.sidhu7@gmail.com>
2015-05-27 13:17:33 +0530

    add new expense controller

---
+++ client/app/components/dashboard/newExpense.controller.js
@@ -0,0 +1,42 @@
+angular.module('expense')
+  .controller('newExpense.controller', NewExpenseController);
+
+function NewExpenseController($state, $http, $scope) {
+  this.parent = $scope.vm;
+  this.$http = $http;
+  this.model = {
+    date: new Date()
+  }
+  this.status = {
+    open: !this.parent.expenses.length
+  }
+  this.dateOptions = {
+    formatYear: 'yy',
+    startingDay: 1,
+    today: new Date()
+  };
+}
+
+NewExpenseController.prototype.createExpense = function() {
+  var vm = this;
+  if (!vm.isValid()) {
+    return;
+  }
+  return vm.$http.post('/api/expenses', vm.model)
+  .then(function(response){
+    if (response && response.status === 200) {
+      vm.model = {
+        date: new Date()
+      };
+      vm.parent.newExpense = true
+    }
+  }, function (error) {
+    console.log(error)
+  })
+}
+NewExpenseController.prototype.isValid = function() {
+  if (!this.model.amount || this.model.amount <= 0) {
+    return false;
+  }
+  return true;
+}
