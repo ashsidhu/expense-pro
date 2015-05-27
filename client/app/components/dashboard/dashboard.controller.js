a1274f7cc9db44576a717ec069c53baf4675d6cf
Ashmeet Sidhu <ashmeet.sidhu7@gmail.com>
2015-05-27 19:42:31 +0530

    dashboard controller for client side

--- client/app/components/dashboard/dashboard.controller.js
+++ client/app/components/dashboard/dashboard.controller.js
@@ -1,6 +1,62 @@
 angular.module('expense')
   .controller('dashboard.controller', DashboardController);
 
-function DashboardController($state, $http) {
-  this.foo='bar'
+function DashboardController($state, $http, $scope) {
+  var vm = this;
+  vm.expenses=[{}];
+  vm.getExpenses = getExpenses
+  vm.updateExpense = updateExpense
+  vm.deleteExpense = deleteExpense
+  vm.newExpense = false
+  vm.filter = {}
+  vm.dateOptions = {
+    formatYear: 'yy',
+    startingDay: 1,
+    today: new Date()
+  };
+  getExpenses();
+  $scope.$watch('vm.newExpense', getExpenses)
+
+  function getExpenses() {
+    var url = getFilterUrl();
+    return $http.get(url)
+    .then(function(response) {
+      if (response && response.status === 200) {
+        vm.newExpense = false;
+        for (var i = 0; i < response.data.data.length; i++) {
+          response.data.data[i].amount = +response.data.data[i].amount
+        }
+        vm.expenses = response.data.data;
+      }
+    })
+  }
+
+  function getFilterUrl() {
+    return 'api/expenses'
+  }
+
+  function updateExpense(id, expense) {
+    return $http.put('api/expenses/' + id, {
+      amount: expense.amount,
+      category: expense.category,
+      comment: expense.comment,
+      description: expense.description,
+      date: expense.date
+    }).then(function(response){
+      if (response && response.status === 200) {
+        expense.updated = true;
+        setTimeout(function() {expense.updated = false;}, 2000)
+      }
+    })
+  }
+
+  function deleteExpense(id) {
+    return $http.delete('api/expenses/' + id)
+    .then(function(response){
+      if (response && response.status === 200) {
+        return getExpenses();
+      }
+    })
+  }
 }
+
